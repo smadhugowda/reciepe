e51c4286bc87fcd20835acf06d3e8cbb3ff9da24
Madhu <smadhu.gowda@gmail.com>
2019-12-23 17:15:18 +0530

    Initial Commit

---
+++ src/test/java/guru/springframework/reciepe/ReciepeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.reciepe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciepeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
