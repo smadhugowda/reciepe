78b0f3d5cf85a4895a3c5c1e709984efb4ba2bd8
Madhu <smadhu.gowda@gmail.com>
2019-12-27 13:03:15 +0530

    Added enum difficulty

--- src/main/java/guru/springframework/recipe/domain/Notes.java
+++ src/main/java/guru/springframework/recipe/domain/Notes.java
@@ -1,11 +1,13 @@
 package guru.springframework.recipe.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 
+@Entity
 public class Notes {
 
 	@Id
