4f97e2b34ebbd6907abd3f617652cfa73e58f5fd
Madhu <smadhu.gowda@gmail.com>
2019-12-30 11:58:36 +0530

    JPA Bidirectional

--- src/main/java/guru/springframework/recipe/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipe/domain/Ingredient.java
@@ -1,30 +1,33 @@
 package guru.springframework.recipe.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-
+/**
+ * Created by jt on 6/13/17.
+ */
 @Entity
 public class Ingredient {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String description;
-	private BigDecimal amount;
-	
-	@ManyToOne
-	private Recipe recipe;
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
 
     public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
     }
 
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
@@ -33,48 +36,44 @@ public class Ingredient {
         this.uom = uom;
         this.recipe = recipe;
     }
-	
-	@OneToOne(fetch = FetchType.EAGER)
-	private UnitOfMeasure uom;
 
-	public UnitOfMeasure getUom() {
-		return uom;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setUom(UnitOfMeasure uom) {
-		this.uom = uom;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public BigDecimal getAmount() {
+        return amount;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 
-	public BigDecimal getAmount() {
-		return amount;
-	}
+    public Recipe getRecipe() {
+        return recipe;
+    }
 
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
 
-	public Recipe getRecipe() {
-		return recipe;
-	}
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
 
-	public void setRecipe(Recipe recipe) {
-		this.recipe = recipe;
-	}
-	
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
 }
